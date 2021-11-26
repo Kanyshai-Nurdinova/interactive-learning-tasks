ac599d7a1799e6f3d75d96d10f61b664f313a58d
Kanyshai-Nurdinova <kanini1994@gmail.com>
2021-11-26 04:52:56 +0000

    added more resources

--- vpc/public_subnet.tf
+++ vpc/public_subnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "public_subnet1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.101.0/24"
-  availability_zone = data.aws_availability_zones.available.names[0]
+  availability_zone = "eu-west-1a"
 
   tags = {
     Team = "DevOps"
@@ -12,8 +12,7 @@ resource "aws_subnet" "public_subnet1" {
 resource "aws_subnet" "public_subnet2" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.102.0/24"
-  availability_zone = data.aws_availability_zones.available.names[1]
-
+  availability_zone = "eu-west-1b"
   tags = {
     Team = "DevOps"
     Environment = "Dev"
@@ -23,7 +22,7 @@ resource "aws_subnet" "public_subnet2" {
 resource "aws_subnet" "public_subnet3" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.103.0/24"
-  availability_zone = data.aws_availability_zones.available.names[2]
+  availability_zone = "eu-west-1c"
 
   tags = {
     Team = "DevOps"
